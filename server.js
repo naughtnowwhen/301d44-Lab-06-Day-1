4a4605c4ab827b553ffae3ecc9256bb050ae3e6e
Dana Voros <vorosdana@gmail.com>
2019-02-19 10:55:10 -0800

    Adds proper location constructor

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict';
 
 require('dotenv').config();
-const cors = require('cors');
+// const cors = require('cors');
 
-let geo = require('./data/geo.json');
+
 
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 
 
 app.get('/location', (request, response) => {
@@ -16,17 +17,17 @@ app.get('/location', (request, response) => {
 })
 
 function searchToLatLong(place) {
-  return {
-    latitude: geo.results[0].geometry.location.lat,
-    longitude: geo.results[0].geometry.location.lng
-  }
+  let geo = require('./data/geo.json');
+  const location = new Location(place, geo);
+  return location;
 }
 
-searchToLatLong(); 
-let asking = searchToLatLong();
-console.log(asking);
-
-
+function Location(query, res) {
+  this.search_query = query;
+  this.formatted_query = res.results[0].formatted_address;
+  this.latitude = res.results[0].geometry.location.lat;
+  this.longitude = res.results[0].geometry.location.lng;
+}
 
 //TODO:
 // well want some error handling;
